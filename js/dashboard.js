c697ae703575cf5867c2c0b1f7cc098bfa2e9011
Ian De La Cruz <IanDeLaCruz@Romells-MacBook-Pro.local>
2016-02-28 09:06:20 +0800

    Commits for saving objects

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,4 @@
+'use strict';
 Parse.initialize('M7rEpExhQwGyQFhORep60981O9FF83c66vj8SQ1L','LVdjgbIgxWhoqazEsjDTRMTWGKxPPBRzsjuDYKKe');
 var logout = document.getElementById('logout');
 logout.addEventListener('click', function(){
@@ -6,13 +7,33 @@ logout.addEventListener('click', function(){
   });
 });
 
+
 var createTask = function(){
-  var taskName = document.getElementById('task_name');
+  var taskName = document.getElementById('task_name').value;
   var taskType_input = document.getElementById('task_type');
   var taskType = taskType_input.options[taskType_input.selectedIndex].text;
   var taskDiff_input = document.getElementById('diff_level');
   var taskDiff = taskDiff_input.options[taskDiff_input.selectedIndex].value;
   var prioLvl_input = document.getElementById('prio_level');
   var prioLvl = prioLvl_input.options[prioLvl_input.selectedIndex].value;
-  var t = new Task();
+  var deadline = document.getElementById('datepicker').value;
+  var currDate = new Date();
+  console.log(typeof taskName);
+  var task = new Task();
+  task.set("dayAssigned", currDate);
+  task.set('name', taskName);
+  task.set('deadline', )
+  task.save({
+    success:function(res){
+      console.log("Hi");
+    },
+    error:function(res,error){
+      console.log("Error: " + error.code + " " + error.message);
+    }
+  });
 };
+var createBtn = document.getElementById('add');
+createBtn.addEventListener('click', function(event){
+  event.preventDefault();
+  createTask();
+});
